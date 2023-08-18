6a4d19822409eeafccec5588e8ac4505175e7483
omerfdev <omerfdev@gmail.com>
2023-08-18 23:26:28 +0300

    MVC

---
+++ AlgorithmsIdMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AlgorithmsIdMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
